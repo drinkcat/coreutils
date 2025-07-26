593114599375815e610820f30de68792947e2c32
Nicolas Boichat <nicolas@boichat.ch>
2025-07-26 23:08:30 +0800

    fsext: Fix metadata_get_time for change time (ctime) before 1970
    Useful for archeologists and time travellers, do not crash if
    ctime is before 1970.
    
    Creating a file with such a change time is a bit challenging
    (touch can't change that), so we can't easily add end-to-end
    tests.
    
    Steps to create a filesystem with a file `x` with birth/change
    time in 1960.
    ```
    dd if=/dev/zero bs=100M count=0 seek=1 of=ext4
    mkfs.ext4 ext4
    sudo mount ext4 mnt
    touch mnt/x
    umount mnt
    echo "set_inode_field x ctime 196001010101" | debugfs -w ext4
    echo "set_inode_field x ctime_extra 1234" | debugfs -w ext4
    echo "set_inode_field x crtime 196001010101" | debugfs -w ext4
    echo "set_inode_field x crtime_extra 0x123400" | debugfs -w ext4
    sudo mount ext4 mnt
    ```
    
    $ cargo run -p uu_stat mnt/x
      File: mnt/x
      size: 0         	Blocks: 0          IO Block: 1024   regular empty file
    Device: 700h/1792d	Inode: 13          Links: 1
    Access: (0644/-rw-r--r--)  Uid: (    0/    root)   Gid: (    0/    root)
    Access: 2025-07-26 19:52:00.849821694 +0800
    Modify: 2025-07-26 19:52:00.849821694 +0800
    Change: 1960-01-02 09:01:00.000298240 +0800
     Birth: 1960-01-02 09:01:00.000298240 +0800

--- src/uucore/src/lib/features/fsext.rs
+++ src/uucore/src/lib/features/fsext.rs
@@ -128,8 +128,20 @@ pub enum MetadataTimeField {
 pub fn metadata_get_time(md: &Metadata, md_time: MetadataTimeField) -> Option<SystemTime> {
     match md_time {
         MetadataTimeField::Change => {
-            // TODO: This is incorrect for negative timestamps.
-            Some(UNIX_EPOCH + Duration::new(md.ctime() as u64, md.ctime_nsec() as u32))
+            let mut st = UNIX_EPOCH;
+            let (secs, nsecs) = (md.ctime(), md.ctime_nsec());
+            if secs >= 0 {
+                st += Duration::from_secs(secs as u64);
+            } else {
+                st -= Duration::from_secs(-secs as u64);
+            }
+            if nsecs >= 0 {
+                st += Duration::from_nanos(nsecs as u64);
+            } else {
+                // Probably never the case, but cover just in case.
+                st -= Duration::from_nanos(-nsecs as u64);
+            }
+            Some(st)
         }
         MetadataTimeField::Modification => md.modified().ok(),
         MetadataTimeField::Access => md.accessed().ok(),
